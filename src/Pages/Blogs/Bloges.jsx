4d2dbaea2d5344bbd2833ff66202cdb01bcf16f4
webdevtamim <tamimahmed12.bd@gmail.com>
2023-12-17 11:38:58 +0600

    second commit

--- src/Pages/Blogs/Bloges.jsx
+++ src/Pages/Blogs/Bloges.jsx
@@ -1,7 +1,7 @@
 const Bloges = () => {
     return (
         <div>
-            <h2 className="text-3xl font-semibold text-center text-headning-color py-40">Blogs</h2>
+            <h2 className="text-3xl font-semibold text-center text-headning-color py-40">Blogs page</h2>
         </div>
     );
 };
